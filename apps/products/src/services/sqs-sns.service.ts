9728075f1c9ef8fa73c4b4ce575c1375f52c49f6
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-03 22:37:45 +0300

    fix

--- apps/products/src/services/sqs-sns.service.ts
+++ apps/products/src/services/sqs-sns.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable } from '@nestjs/common';
 import { SNS } from 'aws-sdk';
 import { SQSEvent } from 'aws-lambda';
 import { validate } from 'class-validator';
@@ -8,12 +8,15 @@ import { Repository } from 'typeorm';
 import { winstonLogger } from '@libs/utils';
 import { Product } from '@libs/entities';
 import { CreateProductBodyDTO } from '../dtos/products.dtos';
+import { ProductsService } from './products.service';
 
 const CONFIG = process.env;
 
 @Injectable()
 export class SqsSnsService {
 	private readonly logger = winstonLogger;
+	
+	@Inject() private readonly productService: ProductsService;
 	@InjectRepository(Product) private readonly productRepository: Repository<Product>;
 	
 	private readonly sns = new SNS({
@@ -22,42 +25,61 @@ export class SqsSnsService {
 	});
 	
 	async catalogBatchProcess(event: SQSEvent): Promise<void> {
-		const productsToSave: Product[] = [];
-		const invalidProducts: Product[] = [];
+		const validProducts: CreateProductBodyDTO[] = [];
+		const invalidProducts: CreateProductBodyDTO[] = [];
 		await event.Records.reduce(async (promise, record) => {
 			await promise;
 			const rawProduct = JSON.parse(record.body);
 			const product = rawProduct ? new CreateProductBodyDTO(rawProduct) : rawProduct;
 			const errors = await validate(product);
 			if (!errors.length) {
-				productsToSave.push(product);
+				validProducts.push(product);
 			} else {
 				invalidProducts.push(rawProduct);
 			}
 		}, Promise.resolve());
+		const validProductsTitles = validProducts.map((product) => product.title);
+		const invalidProductsTitles = invalidProducts.map((product) => product.title);
 		if (invalidProducts.length > 0) {
-			this.logger.logRequest(`Invalid products: ${JSON.stringify(invalidProducts)}`);
+			this.logger.logRequest(`Invalid products: ${validProducts}`);
+		} else if (validProducts.length > 0) {
+			await validProducts.reduce(async (promise, product) => {
+				await promise;
+				await this.productService.createProduct(product);
+			}, Promise.resolve());
+			this.logger.logRequest(`Success saved products: ${validProductsTitles}`);
 		}
-		if (productsToSave.length > 0) {
-			await this.productRepository.insert(productsToSave);
-			const productsTitles = productsToSave.map(({ title }) => title).join(',');
-			this.logger.logRequest(`Success save products: ${productsTitles}`);
-			await this.completionOfProductsProcessing(productsTitles);
+		if (invalidProductsTitles.length) {
+			await this.completionOfProductsProcessing({ validProducts: [], invalidProducts: invalidProductsTitles });
+		}
+		if (validProductsTitles.length) {
+			await this.completionOfProductsProcessing({ validProducts: validProductsTitles, invalidProducts: [] });
 		}
 	}
 	
-	private completionOfProductsProcessing(productsTitles: string): Promise<void> {
+	private completionOfProductsProcessing({ validProducts, invalidProducts }: {
+		invalidProducts: string[],
+		validProducts: string[],
+	}): Promise<void> {
+		const isValidProduct = validProducts.length > 0;
+		this.logger.logRequest(`isValidProduct: ${isValidProduct}`);
 		return new Promise((resolve, reject) => {
 			this.sns.publish({
 				Subject: 'products from csv file were saved',
-				Message: `products name: ${productsTitles}`,
+				Message: `products: ${isValidProduct ? `valid ${validProducts}` : `invalid ${invalidProducts}`}`,
 				TopicArn: CONFIG.SNS_ARN,
+				MessageAttributes: {
+					isValidProduct: {
+						DataType: 'String',
+						StringValue: `${isValidProduct}`,
+					},
+				}
 			}, (error) => {
 				if (error) {
 					this.logger.logError(`error sns send: ${error}`);
 					reject(error);
 				} else {
-					this.logger.logRequest('success send to sns');
+					this.logger.logRequest('success send to sns change');
 					resolve();
 				}
 			});
