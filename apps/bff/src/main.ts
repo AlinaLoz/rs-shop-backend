628909b8dad6b4f23550e644b00ea0dcbcfa9196
a.lozyuk <a.lozyuk@pixelplex.io>
2021-11-05 11:09:22 +0300

    [task-9] Backend For Frontend

---
+++ apps/bff/src/main.ts
@@ -0,0 +1,41 @@
+require('dotenv').config();
+import { HttpStatus } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Handler, APIGatewayEvent, S3Event } from 'aws-lambda';
+
+import { errorResponse, successResponse } from '@libs/utils';
+import { ERRORS } from '@libs/constants';
+import { ImportsModule } from './imports.module';
+import { ImportsService } from './services/imports.service';
+
+export const importProductsFile: Handler = async (
+	event: APIGatewayEvent,
+) => {
+	if (!/\.csv$/.test(event.queryStringParameters.name)) {
+		return errorResponse(new Error(ERRORS.NOT_A_CSV), HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+	
+	const appContext = await NestFactory.createApplicationContext(ImportsModule);
+	const importsService = appContext.get(ImportsService);
+	
+	try {
+		const body = await importsService.importProductsFile(event.queryStringParameters.name);
+		return successResponse(body);
+	} catch (err) {
+		return errorResponse(err);
+	}
+};
+
+export const uploadProductsFile: Handler = async (
+	event: S3Event,
+) => {
+	const appContext = await NestFactory.createApplicationContext(ImportsModule);
+	const importsService = appContext.get(ImportsService);
+	
+	try {
+		const countProducts = await importsService.uploadProductsFile(event);
+		return successResponse({ countProducts });
+	} catch (err) {
+		return errorResponse(err);
+	}
+};
